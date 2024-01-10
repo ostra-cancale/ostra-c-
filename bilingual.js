caada34fb1ca7e6f397361f73b6370eefeea7c2c
ostra-cancale <elsa.dlvt@hotmail.fr>
2024-01-10 14:19:18 +0100

    modifs

--- bilingual.js
+++ bilingual.js
@@ -9,6 +9,19 @@ const buttonbookFra = document.getElementById("buttonbookFr");
 const buttonbookEng = document.getElementById("buttonbookEn");
 const prestextFra = document.getElementById("presentationtext");
 const prestextEng = document.getElementById("presentationtextEn");
+const appartpresFra = document.querySelector(".appartpres");
+const appartpresEng = document.querySelector(".appartpresEn");
+const apparttitleEng = document.querySelector(".apparttitleEn");
+const apparttitleFra = document.querySelector(".apparttitle");
+const privateParkEng = document.querySelector("#privateParkEn");
+const privateParkFra = document.querySelector("#privateParkFr");
+const bigButtonEng = document.querySelector(".bigButtonEn");
+const bigButtonFra = document.querySelector(".bigButton");
+
+const apparttitle2Eng = document.querySelector(".apparttitle2En");
+const apparttitle2Fra = document.querySelector(".apparttitle2");
+
+
 
 
 
@@ -57,7 +70,64 @@ buttonLangChange.addEventListener("click", () => {
   });
 
 
+  buttonLangChange.addEventListener("click", () => {
+    if (getComputedStyle(appartpresEng).display != "none") {
+      appartpresEng.style.display = "none";
+      appartpresFra.style.display = "flex";
+    } else {
+      appartpresEng.style.display = "flex";
+      appartpresFra.style.display = "none";
+    }
+  });
+  
 
+
+  buttonLangChange.addEventListener("click", () => {
+    if (getComputedStyle(apparttitleEng).display != "none") {
+      apparttitleEng.style.display = "none";
+      apparttitleFra.style.display = "flex";
+    } else {
+      apparttitleEng.style.display = "flex";
+      apparttitleFra.style.display = "none";
+    }
+  });
+
+
+
+  buttonLangChange.addEventListener("click", () => {
+    if (getComputedStyle(privateParkEng).display != "none") {
+      privateParkEng.style.display = "none";
+      privateParkFra.style.display = "flex";
+    } else {
+      privateParkEng.style.display = "flex";
+      privateParkFra.style.display = "none";
+    }
+  });
+
+  buttonLangChange.addEventListener("click", () => {
+    if (getComputedStyle(bigButtonEng).display != "none") {
+      bigButtonEng.style.display = "none";
+      bigButtonFra.style.display = "flex";
+    } else {
+      bigButtonEng.style.display = "flex";
+      bigButtonFra.style.display = "none";
+    }
+  });
+
+  buttonLangChange.addEventListener("click", () => {
+    if (getComputedStyle(apparttitle2Eng).display != "none") {
+      apparttitle2Eng.style.display = "none";
+      apparttitle2Fra.style.display = "flex";
+    } else {
+      apparttitle2Eng.style.display = "flex";
+      apparttitle2Fra.style.display = "none";
+    }
+  });
+
+
+
+
+  
 // buttonLangFr.addEventListener("click", () => {
 //     if (getComputedStyle(navEng).display != "none") {
 //       navEng.style.display = "none";
